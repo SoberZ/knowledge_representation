b56d4b02922d1b3ca12a8aef5adf5c5211b99740
Silver Lee-A-Fong <silverlee2000@gmail.com>
2022-11-20 15:45:27 +0100

    Working DPLL and added stopwatch

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -3,6 +3,7 @@ import shutil
 from functools import reduce
 import sys
 import random
+import time
 
 
 def read_sudoku(file, n_lines=-1):
@@ -23,7 +24,7 @@ def read_sudoku(file, n_lines=-1):
                 n_lines -= 1
 
             line = line.replace("\n", "")
-            sudokus.append([0 if c == "." else int(c) for c in line])
+            sudokus.append(['0' if c == "." else c for c in line])
 
     return sudokus
 
@@ -68,7 +69,7 @@ def sudoku2DIMACS(sudokus, N, sudfile):
         for i in range(1, N+1):
             for j in range(1, N+1):
                 val = sudoku[(i-1)*N+(j-1)]  # index is r*N+c
-                if val != 0:
+                if val != '0':
                     values.append(str(i)+str(j)+str(val))
 
         # Create the new .cnf file for the sudoku.
@@ -127,7 +128,7 @@ def evaluate_expression(clauses):
 
     # There is a clause that isn't a literal.
     for clause in clauses:
-        if len(clause) > 2:
+        if len(clause) > 1:
             return 0
 
     return True
@@ -137,42 +138,52 @@ def get_variables(clauses):
     """
     Obtain all the positions that are in the CNF.
     """
-    allPositions = set()
+    allPositions = []
     for clause in clauses:
         for position in clause:
-            allPositions.add(position)
-    return sorted(allPositions)
+            allPositions += [position]
+    return list(set(allPositions))
 
 
-def BCP(clauses):
+def negate_unit(unit):
     """
-    Boolean Constraint Propagation
-    - Every clause (other than the clause itself) containing l is removed (the clause is satisfied if l is).
-    - In every clause that contains -l this literal is deleted (-l can not contribute to it being satisfied)
+    Negation of a string unit.
     """
-    # Obtain literals that occur in the clauses
-    literals = [clause for clause in clauses if len(clause) == 1]
-    literals = reduce(lambda a, b: a.union(b), literals)  # Reduce list of sets
-    already_neg_literals = set(
-        [literal for literal in literals if literal[0] == "-"])  # Negative literals
-    # Negative literals
-    neg_literals = set(
-        ["-" + literal for literal in literals if literal[0] != "-"])
-    neg_literals = neg_literals.union(already_neg_literals)
+    return unit[1:] if unit[0] == "-" else "-"+unit
 
+
+def single_BCP(clauses, unit):
     new_clauses = []
     for clause in clauses:
-        # Skip/remove clause if contains literal
-        if element_in_clause(clause, literals):
+        # Whole clause is true, therefore delete
+        if unit in clause:
             continue
 
-        # Delete the literals that occur in the clause.
-        if element_in_clause(clause, neg_literals):
-            new_clauses.append(clause.difference(neg_literals))
+        # Negative of the unit is in clause, remove unit
+        negation_unit = negate_unit(unit)
+        if negation_unit in clause:
+            new_clause = [x for x in clause if x != negation_unit]
+            if not new_clause:
+                return -1
+            new_clauses.append(new_clause)
         else:
             new_clauses.append(clause)
-
     return new_clauses
+
+
+def unit_prop(clauses):
+    assignment = []
+    unit_clauses = [clause for clause in clauses if len(clause) == 1]
+    while unit_clauses:
+        unit = unit_clauses[0]
+        clauses = single_BCP(clauses, unit[0])
+        assignment += [unit[0]]
+        if clauses == -1:
+            return -1, []
+        if not clauses:
+            return clauses, assignment
+        unit_clauses = [clause for clause in clauses if len(clause) == 1]
+    return clauses, assignment
 
 
 def DPLL(clauses, assignment, strategy):
@@ -181,42 +192,29 @@ def DPLL(clauses, assignment, strategy):
 
     DIMACS_file - Sudoku DIMACS file
     """
-    new_clauses = BCP(clauses)
-    evaluate_result = evaluate_expression(new_clauses)
+    new_clauses, new_assign = unit_prop(clauses)
+    assignment += new_assign
 
-    if evaluate_result is True:  # Formula evaluates to True -> return True
-        return assignment
-    elif evaluate_result is False:  # Formula evaluates to False -> return False
+    if new_clauses == -1:
         return []
-    else:  # Evaluate result returns 0 which means formula is undecided.
-        # Choose next unassigned variable
-        all_positions = set(get_variables(clauses))
-        open_positions = sorted(all_positions.difference(assignment))
+    if not new_clauses:
+        return assignment
 
-        if strategy == '-S1':
-            choice = random.choice(open_positions)
-        if strategy == '-S2':
-            choice = jw_os(open_positions, clauses)
-        if strategy == '-S3':
-            choice = moms(open_positions, clauses)
+    all_positions = sorted(get_variables(new_clauses))
 
-        # Return (DPLL with that variable True) || (DPLL with that variable False)
-        solution = None
-        if choice[0] == "-":  # First positive choice
-            new_clauses.append(set([choice[0:]]))
-            solution = DPLL(new_clauses, assignment + [choice[0:]], strategy)
-        else:
-            new_clauses.append(set([choice]))
-            solution = DPLL(new_clauses, assignment + [choice], strategy)
+    # Choose next unassigned variable
+    if strategy == '-S2':
+        choice = jw_os(all_positions, new_clauses)
+    elif strategy == '-S3':
+        choice = moms(all_positions, new_clauses)
+    else: # strategy == '-S1' or default
+        choice = random.choice(all_positions)
 
-        if not solution:  # Negative choice
-            if choice[0] == "-":  # Negative choice has been chosen
-                new_clauses.append(set([choice]))
-                solution = DPLL(new_clauses, assignment + [choice], strategy)
-            else:  # Make choice negative
-                new_clauses.append(set(["-"+choice]))
-                solution = DPLL(new_clauses, assignment +
-                                ["-"+choice], strategy)
+    # Return (DPLL with that variable True) || (DPLL with that variable False)
+    solution = DPLL(new_clauses + [[choice]], assignment + [choice], strategy)
+    if not solution:
+        negation_choice = negate_unit(choice)
+        solution = DPLL(new_clauses + [[negation_choice]], assignment + [negation_choice], strategy)
 
     return solution
 
@@ -232,12 +230,18 @@ def DPLL_Strategy(DIMACS_file, strategy):
         data = f.readlines()
 
     # First line contains N variables and N clauses
-    firstLine, clauses = data[0], data[1:]
-    clauses = [set(clause.split()[:-1]) for clause in clauses]  # Remove zeroes
+    _, clauses = data[0], data[1:]
+    new_clauses = []
+    for clause in clauses:
+        splitted = clause.split()[:-1]  # Remove zeroes
+        new_clause = []
+        for split in splitted:
+            new_clause.append(split)
+        new_clauses.append(new_clause)
 
     # Perform Boolean Constraint Propagation
     assignment = []
-    solution = DPLL(clauses, assignment, strategy)
+    solution = DPLL(new_clauses, assignment, strategy)
 
     return solution
 
@@ -272,15 +276,12 @@ def moms(open_positions, clauses):
     # Initiate used variables
     selected_literal = open_positions[0]
     max_score = 0
-    min_clause_length = 99999
+    min_clause_length = min(clauses, key=lambda x: len(x))
     min_clauses = []
     moms_score = 0
     # Loop over all clauses, find minimal clause length and add those clauses to a list
     for clause in clauses:
-        if len(clause) < min_clause_length:
-            min_clause_length = len(clause)
-            min_clauses = [clause]
-        elif len(clause) == min_clause_length:
+        if len(clause) == min_clause_length:
             min_clauses.append(clause)
     # Loop over all literals in the open_positions to calculate moms score per literal
     for literal in open_positions:
@@ -295,7 +296,8 @@ def moms(open_positions, clauses):
                 neg_literal_count += 1
 
         # Calculate the MOM formula per literal to see if its value is the largest, if so, select it
-        moms_score = ((literal_count + neg_literal_count)* 2**k) + (literal_count*neg_literal_count)
+        moms_score = ((literal_count + neg_literal_count) * 2 **
+                      k) + (literal_count*neg_literal_count)
         if moms_score > max_score:
             max_score = moms_score
             selected_literal = literal
@@ -312,8 +314,9 @@ def sudoku_print(sudoku, N):
         print_sudoku.append(row)
         for j in range(N):
             row.append('0')
-            
+
     for position in sudoku:
+        position = str(position)
         if len(position) == 3:
             if print_sudoku[int(position[0])-1][int(position[1])-1] not in ('0', str(position[2])):
                 print('FOUT IN SUDOKU!! MEERDERE ASSIGNMENTS VOOR DEZELDE PLEKKEN')
@@ -324,12 +327,13 @@ def sudoku_print(sudoku, N):
 
 if __name__ == "__main__":
     # Parse the command line input
-    strategy, file = parse_command()
-
-    sudfile = "./testsets/4x4.txt"
+    strategy, sudfile = parse_command()
+    # strategy = "-S1"
+    # sudfile = "./testsets/top91.sdk.txt"
 
     # Read sudoku from file.
-    sudlist = read_sudoku(sudfile, 5)
+    n_sudokus = 10
+    sudlist = read_sudoku(sudfile, n_sudokus)
 
     # Find N x N dimension for sudoku.
     sudlen = len(sudlist[0])
@@ -338,14 +342,24 @@ if __name__ == "__main__":
     # Read the problem (=clauses) as DIMACS file
     sudoku2DIMACS(sudlist, N, sudfile)
 
-    # Implement DP + two heuristics
-    nth_sudoku = 0
-    parsedFile = sudfile.split("/")[-1].split(".")[0]
-    DIMACS_file = "./sudoku_DIMACS/"+parsedFile + \
-        "/"+parsedFile+"_"+str(nth_sudoku)+".cnf"
 
-    # Strategy based on command line arguments
-    result = DPLL_Strategy(DIMACS_file, strategy)
-    sudoku_print(result, N)
+    time_lapsed = 0
+
+    # Implement DP + two heuristics
+    for i in range(0, len(sudlist)):
+        parsedFile = sudfile.split("/")[-1].split(".")[0]
+        DIMACS_file = "./sudoku_DIMACS/"+parsedFile + \
+            "/"+parsedFile+"_"+str(i)+".cnf"
+
+        start_time = time.time()
+        # Strategy based on command line arguments
+        result = DPLL_Strategy(DIMACS_file, strategy)
+        end_time = time.time()
+
+        time_lapsed += end_time - start_time
+
+        # sudoku_print(result, N)
+
+    print("total time: ", time_lapsed)
 
     # Write output (=variable assignments) as DIMACS file
