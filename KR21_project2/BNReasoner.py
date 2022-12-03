950245eb997ce98e0fcd69bc9d9ccc0699a7f0f7
Dexter Roelofsen <D.W.D.Roelofsen@student.tudelft.nl>
2022-12-03 18:53:03 +0100

    Network_pruning should be working

--- KR21_project2/BNReasoner.py
+++ KR21_project2/BNReasoner.py
@@ -12,29 +12,34 @@ class BNReasoner:
             self.bn = BayesNet()
             # Loads the BN from an BIFXML file
             self.bn.load_from_bifxml(net)
-            print(self.bn.get_cpt('Rain?'))
         else:
             self.bn = net
 
     # TODO: This is where your methods should go
-    def network_pruning(self, variable='Rain?', evidence='False'):
+    def network_pruning(self, variable, evidence):
         """
         :param variable: Variable which has known evidence to prune network with
         :param evidence: Assignment of the variable: True/False
         """
-        print(variable,evidence)
+        # Remove rows from cpt which do not agree with the evidence of the variable
+        for current_var in self.bn.get_all_variables():
+            removing_row_set = set()
+            if variable in self.bn.get_cpt(current_var):
+                for index, row in self.bn.get_cpt(current_var).iterrows():
+                    if str(row[variable]) != str(evidence):
+                        removing_row_set.add(index)
+                for remove_index in removing_row_set:
+                    self.bn.update_cpt(current_var, self.bn.get_cpt(current_var).drop(remove_index))
+                print('current cpt after removing rain=true:')
+                print(self.bn.get_cpt(current_var))
 
-
-        for index, row in self.bn.get_cpt(variable).iterrows():
-            print(self.bn.get_all_cpts())
-            if str(row[variable]) != str(evidence):
-                self.bn.update_cpt(variable, self.bn.get_cpt(variable).drop(index-1))
-        print(self.bn.get_all_cpts())
-        self.bn.all
-        self.bn.del_edge((variable,node2))
-        self.bn.draw_structure()
-        print(self.bn.get_cpt(variable))
-        pass
+        # Remove all edges which have the selected variable as first node (directed)
+        remove_set = set()
+        for node_1, node_2 in self.bn.structure.edges():
+            if node_1 == str(variable):
+                remove_set.add((node_1, node_2))
+        for node_tuple in remove_set:
+            self.bn.del_edge((node_tuple[0], node_tuple[1]))
 
     def marginalization(self):
         pass
