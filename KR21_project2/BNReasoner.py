36dd4ea5f524e48e1e4fa2542fdd3ebd344645af
Dexter Roelofsen <D.W.D.Roelofsen@student.tudelft.nl>
2022-12-03 17:48:17 +0100

    Network_pruning start

--- KR21_project2/BNReasoner.py
+++ KR21_project2/BNReasoner.py
@@ -12,7 +12,37 @@ class BNReasoner:
             self.bn = BayesNet()
             # Loads the BN from an BIFXML file
             self.bn.load_from_bifxml(net)
+            print(self.bn.get_cpt('Rain?'))
         else:
             self.bn = net
 
     # TODO: This is where your methods should go
+    def network_pruning(self, variable='Rain?', evidence='False'):
+        """
+        :param variable: Variable which has known evidence to prune network with
+        :param evidence: Assignment of the variable: True/False
+        """
+        print(variable,evidence)
+
+
+        for index, row in self.bn.get_cpt(variable).iterrows():
+            print(self.bn.get_all_cpts())
+            if str(row[variable]) != str(evidence):
+                self.bn.update_cpt(variable, self.bn.get_cpt(variable).drop(index-1))
+        print(self.bn.get_all_cpts())
+        self.bn.all
+        self.bn.del_edge((variable,node2))
+        self.bn.draw_structure()
+        print(self.bn.get_cpt(variable))
+        pass
+
+    def marginalization(self):
+        pass
+
+    def maxing_out(self):
+        pass
+
+
+if __name__ == "__main__":
+    BN = BNReasoner('testing/lecture_example.BIFXML')
+    BN.network_pruning('Rain?', 'False')
