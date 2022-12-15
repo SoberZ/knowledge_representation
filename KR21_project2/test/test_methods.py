729a051fc7ace6da5054fdbfd17956d0540efa19
Silver Lee-A-Fong <silverlee2000@gmail.com>
2022-12-15 18:16:54 +0100

    Latest tests

--- KR21_project2/test/test_methods.py
+++ KR21_project2/test/test_methods.py
@@ -1,8 +1,6 @@
 import unittest
-import sys
 import pandas as pd
 import BNReasoner as BNR
-
 
 
 class MyTestCase(unittest.TestCase):
@@ -23,61 +21,92 @@ class MyTestCase(unittest.TestCase):
         # Setup answer in dataframe to test the method with
         answer_Data = {'Winter?': [False, True], 'p': [1.0, 1.0]}
         test_df = pd.DataFrame(data=answer_Data)
-
         # Create BN from example and perform network_pruning on it
         BN = BNR.BNReasoner('testing/lecture_example.BIFXML')
-        print(BN.bn.get_cpt('Rain?'),'\n\n\n')
         marginalized_BN = BN.marginalization(BN.bn.get_cpt('Rain?'), 'Rain?')
-
-        print(marginalized_BN)
         # Check if pruned dataframe is the same as the correct answer
         for i, j in zip(test_df.iterrows(), marginalized_BN.iterrows()):
             self.assertEqual(str(i[1].to_string()), str(j[1].to_string()))
 
     def test_maxing_out(self):
         # Setup answer in dataframe to test the method with
-        answer_Data = {'Winter?': [False, True], 'p': [0.9, 0.8], 'extended_factors': ['Rain?= False','Rain?= False']}
+        answer_Data = {'Winter?': [False, True], 'p': [0.9, 0.8], 'extended_factors': ['Rain?= False','Rain?= True']}
         test_df = pd.DataFrame(data=answer_Data)
 
         # Create BN from example and perform network_pruning on it
         BN = BNR.BNReasoner('testing/lecture_example.BIFXML')
-        maxed_out_BN = BN.maxing_out('Rain?').bn.get_cpt('Rain?')
-
+        maxed_out_BN = BN.maxing_out(BN.bn.get_cpt('Rain?'), 'Rain?')
         # Check if pruned dataframe is the same as the correct answer
         for i, j in zip(test_df.iterrows(), maxed_out_BN.iterrows()):
             self.assertEqual(str(i[1].to_string()), str(j[1].to_string()))
 
-    def test_d_separation(self):
-        BN = BNR.BNReasoner('testing/lecture_example.BIFXML')
-        response1 = BN.d_separation(['Rain?'], ['Sprinkler?'], ['Winter?'])  # Not d-separated
-        response2 = BN.d_separation(['Rain?'], ['Sprinkler?'], ['Winter?', 'Wet Grass?'])  # d-separated
-        self.assertEqual(response1, False)
-        self.assertEqual(response2, True)
-
-    def test_independence(self):
-        BN = BNR.BNReasoner('testing/lecture_example.BIFXML')
-        response1 = BN.d_separation(['Rain?'], ['Sprinkler?'], ['Winter?'])  # Not d-separated
-        response2 = BN.d_separation(['Rain?'], ['Sprinkler?'], ['Winter?', 'Wet Grass?'])  # d-separated
-        self.assertEqual(response1, False)
-        self.assertEqual(response2, True)
+    # def test_d_separation(self):
+    #     BN = BNR.BNReasoner('testing/lecture_example.BIFXML')
+    #     response1 = BN.d_separation(['Rain?'], ['Sprinkler?'], ['Winter?'])  # Not d-separated
+    #     response2 = BN.d_separation(['Rain?'], ['Sprinkler?'], ['Winter?', 'Wet Grass?'])  # d-separated
+    #     self.assertEqual(response1, False)
+    #     self.assertEqual(response2, True)
+    #
+    # def test_independence(self):
+    #     BN = BNR.BNReasoner('testing/lecture_example.BIFXML')
+    #     response1 = BN.d_separation(['Rain?'], ['Sprinkler?'], ['Winter?'])  # Not d-separated
+    #     response2 = BN.d_separation(['Rain?'], ['Sprinkler?'], ['Winter?', 'Wet Grass?'])  # d-separated
+    #     self.assertEqual(response1, False)
+    #     self.assertEqual(response2, True)
 
     def test_factor_multiplication(self):
-        pass
+        # Initialising example and running method
+        BN = BNR.BNReasoner('testing/lecture_example.BIFXML')
+        df1 = BN.bn.get_cpt('Rain?')
+        df2 = BN.bn.get_cpt('Slippery Road?')
+        outcome = BN.factor_multiplication(df1,df2)
+        # Expected DataFrame
+        answer_Data = {'Winter?': [False, False, True, True, False, False, True, True], 'Rain?':
+            [False, False, False, False, True, True, True, True], 'Slippery Road?':
+            [False, True, False, True, False, True, False, True], 'p':
+            [0.90, 0.00, 0.20, 0.00, 0.03, 0.07, 0.24, 0.56]}
+        expected = pd.DataFrame(data=answer_Data)
+
+        for i, j in zip(expected.iterrows(), outcome.iterrows()):
+            self.assertEqual(str(i[1].to_string()), str(j[1].to_string()))
 
     def test_variable_elimination(self):
-        pass
+        # initiate dataframe and use method for its outcome
+        BN = BNR.BNReasoner('testing/dog_problem.BIFXML')
+        outcome = BN.variable_elimination(["family-out", "dog-out", "light-on"])
+        # setup correct answer in dataframe
+        answer_Data = {'bowel-problem': [False, False, True, True],'hear-bark':
+            [False, True, False, True], 'p': [0.640315, 0.359685, 0.286345, 0.713655]}
+        expected = pd.DataFrame(data=answer_Data)
+        # check for each row if the outcome equals the expected outcome
+        for i, j in zip(expected.iterrows(), outcome.iterrows()):
+            self.assertEqual(str(i[1].to_string()), str(j[1].to_string()))
 
     def test_most_probable_explanation(self):
         # Load lecture example
         BN = BNR.BNReasoner('testing/lecture_example.BIFXML')
+        # Run method with a test_evidence dictionary
         test_evidence = {'Rain?': True, 'Winter?': False}
-        # Determine correct output to compare with
-        correct_answer = ({'Rain?': 'True', 'Sprinkler?': 'True', 'Wet Grass?': 'True'}, 'p=0.95')
-        # Run comparison
-        self.assertEqual(BN.most_probable_explanation(test_evidence), correct_answer)
+        test_outcome = (BN.most_probable_explanation(test_evidence)[0])
+        # Test the outcome of the method against the correct outcome
+        correct_answer = {'Rain?': True, 'Wet Grass?': 'True', 'Sprinkler?': 'True', 'Winter?': False}
+        self.assertEqual(test_outcome,correct_answer)
+        # Test the probability
+        correct_p_value = 'p=0.95'
+        self.assertEqual(correct_p_value, BN.most_probable_explanation(test_evidence)[1])
 
     def test_ordering(self):
-        pass
+        # Load lecture example
+        BN = BNR.BNReasoner('testing/lecture_example.BIFXML')
+        # Run method for both heuristics
+        test_case_1 = BN.ordering('min-degree')
+        test_case_2 = BN.ordering('min-fill')
+        # Expected orders
+        expected_result_1 = ['Slippery Road?', 'Winter?', 'Sprinkler?', 'Rain?', 'Wet Grass?']
+        expected_result_2 = ['Winter?', 'Sprinkler?', 'Wet Grass?', 'Rain?', 'Slippery Road?']
+        # Test if the methods are correct
+        self.assertEqual(test_case_1, expected_result_1)
+        self.assertEqual(test_case_2, expected_result_2)
 
     def test_marginal_distributions(self):
         pass
